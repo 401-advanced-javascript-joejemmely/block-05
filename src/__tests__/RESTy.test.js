d7756849c8540096e25f036d4f9886ba2bac68e4
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-24 16:41:03 -0700

    Add test

--- src/__tests__/RESTy.test.js
+++ src/__tests__/RESTy.test.js
@@ -6,5 +6,11 @@ describe('RESTy', () => {
     const mountedSimple = shallow(<RESTy />);
     expect(mountedSimple.find('main')).toBeTruthy();
   });
-  it('state change', () => {});
+
+  test('state change', () => {
+    const mountedForm = mount(<RESTy />);
+    const input = mountedForm.find('input').at(0);
+    input.simulate('change', { target: { value: 'https://swapi.co/api/people' } });
+    expect(mountedForm.state('url')).toEqual('https://swapi.co/api/people');
+  });
 });
