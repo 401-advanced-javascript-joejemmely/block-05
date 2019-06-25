5f0e42d5d3d911aeb6b14716fedb0464f7762e38
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-25 08:45:49 -0700

    More tests

--- src/__tests__/RESTy.test.js
+++ src/__tests__/RESTy.test.js
@@ -7,10 +7,24 @@ describe('RESTy', () => {
     expect(mountedSimple.find('main')).toBeTruthy();
   });
 
-  test('state change', () => {
+  test('url state change', () => {
     const mountedForm = mount(<RESTy />);
     const input = mountedForm.find('input').at(0);
     input.simulate('change', { target: { value: 'https://swapi.co/api/people' } });
     expect(mountedForm.state('url')).toEqual('https://swapi.co/api/people');
+  });
+
+  test('POST method state change', () => {
+    const mountedForm = mount(<RESTy />);
+    const input = mountedForm.find('input').at(2);
+    input.simulate('change');
+    expect(mountedForm.state('method')).toEqual('post');
+  });
+
+  test('PUT method state change', () => {
+    const mountedForm = mount(<RESTy />);
+    const input = mountedForm.find('input').at(3);
+    input.simulate('change');
+    expect(mountedForm.state('method')).toEqual('put');
   });
 });
